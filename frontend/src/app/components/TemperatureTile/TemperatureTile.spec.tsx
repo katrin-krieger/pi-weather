e6dfc5f99eabef332be8dc6d08ec929789e6ff24
Krieger, Katrin <Katrin.Krieger@jungheinrich.de>
2024-02-08 15:11:48 +0100

    feat: first pw test

--- frontend/src/app/components/TemperatureTile/TemperatureTile.spec.tsx
+++ frontend/src/app/components/TemperatureTile/TemperatureTile.spec.tsx
@@ -1,7 +1,17 @@
-import {test} from "playwright/test";
+import { test, expect } from "@playwright/experimental-ct-react";
+import TemperatureTileComponent from "./TemperatureTile";
 
-    test("TemperatureTile", () =>{
-            
-        }
-    )
+test("TemperatureTile", async ({ mount }) => {
+  let clicked = false;
 
+  // Mount a component. Returns locator pointing to the component.
+  const component = await mount(
+    <TemperatureTileComponent
+      temperature={0}
+      location={"Magdeburg"}
+    ></TemperatureTileComponent>
+  );
+
+  // As with any Playwright test, assert locator text.
+  await expect(component).toContainText("Magdeburg");
+});
